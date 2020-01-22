c17795ca59c0d45303002258aa93dc0dc0f9a85e
lisa <lisa.ng8@hotmail.com>
2020-01-22 16:49:50 +1100

    Updates

---
+++ precache-manifest.13c2ef9f441b69b31e18a74b3d65f8b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09739056ea2a957e23ce6ee44e762ffd",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "e5089e4e54ff52808956",
+    "url": "/javascript-calculator/static/css/main.5411c801.chunk.css"
+  },
+  {
+    "revision": "9c6fd54f12299c35e15a",
+    "url": "/javascript-calculator/static/js/2.059620f3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/javascript-calculator/static/js/2.059620f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5089e4e54ff52808956",
+    "url": "/javascript-calculator/static/js/main.784c41f8.chunk.js"
+  },
+  {
+    "revision": "054eac1b9e1be10a5c81",
+    "url": "/javascript-calculator/static/js/runtime-main.aafb8b0a.js"
+  }
+]);
